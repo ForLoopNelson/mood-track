2ff59d231bbb9b12c2f8cc41de92bab1a331e3f1
Cory Nelson <fullnelsontech@gmail.com>
2022-09-10 13:12:40 -0400

    First Commit routes folder

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require("../middleware/auth")
+
+const Story = require("../models/Story")
+
+//Login/Landing Page
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", {
+    layout: "login",
+  })
+})
+
+//Dashboard
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean()
+    console.log(req.user.id)
+    console.log(stories)
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
+  }
+})
+
+module.exports = router
