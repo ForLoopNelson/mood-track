6ae0570975c8f8c21395b17bcb8cfa04235448a5
Cory Nelson <fullnelsontech@gmail.com>
2022-09-21 16:52:01 -0400

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const exphbs = require("express-handlebars")
-
 const methodOverride = require("method-override")
 const passport = require("passport")
 const session = require("express-session")
