f94bfc31fb83f88f3612c47832561f0f36efb476
Cory Nelson <fullnelsontech@gmail.com>
2023-01-30 17:51:31 -0500

    added timestamps to schema

--- models/story.js
+++ models/story.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose")
 
-const StorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    //trims any whitespace
-    trim: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    default: "public",
-    enum: ["public", "private"],
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    //!Change: this field should be required because the app will break if the user is not present.
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-})
+const StorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      //trims any whitespace
+      trim: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "public",
+      enum: ["public", "private"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      //!Change: this field should be required because the app will break if the user is not present.
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }, //attempt timeStamp
+  { timestamps: true }
+)
 
 module.exports = mongoose.model("Story", StorySchema)
