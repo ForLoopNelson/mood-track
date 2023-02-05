8c9ef951c76eb14cdc4f3a290c9cd16ab271b7fe
Cory Nelson <fullnelsontech@gmail.com>
2023-02-05 13:24:54 -0500

    fixed model story in index and stories to lowercase

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require("../middleware/auth")
 
-const Story = require("../models/Story")
+const Story = require("../models/story")
 
 //Login/Landing Page
 router.get("/", ensureGuest, (req, res) => {
