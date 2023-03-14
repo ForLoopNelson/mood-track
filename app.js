504db9f0a58f96fb1b71571c75cac5b6709957e2
Cory Nelson <fullnelsontech@gmail.com>
2023-03-14 16:18:33 -0400

    removed unneeded app use

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function (req, res, next) {
 })
 
 // static folder
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 // Routes
 app.use("/", require("./routes/index"))
