412e257afa78ab722c54fe56ea1bf9200279fcfc
Cory Nelson <fullnelsontech@gmail.com>
2024-09-25 14:06:59 -0400

    remved useless time conversion

--- models/story.js
+++ models/story.js
@@ -28,13 +28,6 @@ const StorySchema = new mongoose.Schema(
   { timestamps: true }
 )
 
-// Pre-save hook to set createdAtLocal to local time
-StorySchema.pre('save', function (next) {
-  // Convert the createdAt UTC timestamp to local time before saving
-  const utcTime = this.createdAt || new Date(); // createdAt is automatically set by timestamps: true
-  const offset = -4 * 60 * 60 * 1000; // Adjust this to match your timezone offset (e.g., UTC-4 hours for EDT)
-  this.createdAtLocal = new Date(utcTime.getTime() + offset);
-  next();
-});
+
 
 module.exports = mongoose.model("Story", StorySchema)
